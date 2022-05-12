59ba33808216eda51854e1bb95bfa517ee1581a1
Syamsul Bahari <syamsul.bahari@edi-indonesia.co.id>
2022-05-12 10:28:03 +0700

    push ini

--- config/database.go
+++ config/database.go
@@ -3,14 +3,14 @@ package config
 import (
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
 func SetDatabase() (*gorm.DB, error) {
 	var dsn = os.Getenv("DB_CONFIG")
-	var Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+	var Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
 
