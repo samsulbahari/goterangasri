ae93da632bfb8ecfa91ce1cb1bdb6c638b37acfa
Syamsul Bahari <syamsul.bahari@edi-indonesia.co.id>
2022-05-12 10:40:09 +0700

    test

--- config/database.go
+++ config/database.go
@@ -1,13 +1,15 @@
 package config
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
 func SetDatabase() (*gorm.DB, error) {
-	var dsn = "host=ec2-54-164-40-66.compute-1.amazonaws.com user=tgegltttehbhck password=536dd6f2a4958f994bd37c27c64d17dc6026a13452acde53f59bc901dc81d825 dbname=dbuvp1i6mcmshg port=5432 sslmode=require TimeZone=Asia/Shanghai"
+	var dsn = os.Getenv("DB_CONFIG")
 	var Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
